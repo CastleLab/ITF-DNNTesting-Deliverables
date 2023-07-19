4244122f1a4f471a486af0dcc453a064224c70fd
maybeLee <maybelee98@gmail.com>
2023-07-20 01:07:14 +0800

    Fix Bug

--- api.py
+++ api.py
@@ -18,8 +18,9 @@ class DNNTest(object):
             # if the image path is a file, invoke detect_single.sh,
             # otherwise it is a directory, invoke detect_parallel_yolov7.sh
             if not img_path.endswith(".jpg"):
-                raise ValueError(f"Invalid image path: {img_path}. The image should be .jpg format.")
-
+                raise ValueError(f"Invalid image format: {img_path}. The image should be .jpg format.")
+            img_path = img_path.replace("MetaHand", "")
+            weights_path = weights_path.replace("MetaHand", "")
             cmd = f"docker exec {self.container_name}  /bin/sh -c 'cd MetaHand && python -u -m scripts.evaluation.detect_single " \
                   f"--img_dir ${img_path} " \
                   f"--weights_path ${weights_path} " \
@@ -34,8 +35,10 @@ class DNNTest(object):
                   f"--confidence ${confidence} " \
                   f"--jobs=8"
             subprocess.call(cmd, shell=True)
-        else:
+        elif os.path.isdir(img_path):
             raise NotImplementedError("Haven't implement the parallel detection.")
+        else:
+            raise ValueError(f"Invalid Image Path: {img_path}")
 
     def train_yolov7(self, train_path, valid_path):
         pass
@@ -48,4 +51,4 @@ if __name__ == "__main__":
     container_name = "DNNTesting"
     dnnTest = DNNTest(container_name)
     # dnnTest.numerical_analysis("TensorFuzz.pbtxt")
-    dnnTest.detect_yolov7("./tools/yolov7/coco/images/val2017/000000289222.jpg", "./tools/yolov7/runs/train/yolov7/weights/best.pt")
+    dnnTest.detect_yolov7("./MetaHand/tools/yolov7/coco/images/val2017/000000289222.jpg", "./MetaHand/tools/yolov7/runs/train/yolov7/weights/best.pt")
