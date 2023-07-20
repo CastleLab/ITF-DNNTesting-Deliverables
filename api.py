33c84bcb23f867f1a8c13ea9f1dc82967bf77e2f
maybeLee <maybelee98@gmail.com>
2023-07-20 18:11:05 +0800

    Update training scripts, fix bug

--- api.py
+++ api.py
@@ -15,17 +15,17 @@ class DNNTest(object):
 
     def detect_yolov7(self, img_path, weights_path, size=320, confidence=0.25):
         cmd = f"docker exec {self.container_name}  /bin/sh -c 'cd MetaHand && python -u -m scripts.evaluation.detect_parallel_yolov7" \
-              f"--img_dir ${img_path} " \
-              f"--weights_path ${weights_path} " \
-              f"--size ${size} " \
-              f"--confidence ${confidence} " \
+              f"--img_dir {img_path} " \
+              f"--weights_path {weights_path} " \
+              f"--size {size} " \
+              f"--confidence {confidence} " \
               f"--jobs=8'"
 
         cmd = f"cd MetaHand && python -u -m scripts.evaluation.detect_parallel_yolov7 " \
-              f"--img_dir ${img_path} " \
-              f"--weights_path ${weights_path} " \
-              f"--size ${size} " \
-              f"--confidence ${confidence} " \
+              f"--img_dir {img_path} " \
+              f"--weights_path {weights_path} " \
+              f"--size {size} " \
+              f"--confidence {confidence} " \
               f"--jobs=8"
         subprocess.call(cmd, shell=True)
 
@@ -35,14 +35,14 @@ class DNNTest(object):
             raise ValueError(f"The data path: {data_path} does not exist!")
         cmd = f"docker exec {self.container_name}  /bin/sh -c 'cd MetaHand && python -m torch.distributed.launch " \
               f"--nproc_per_node 3 --master_port 9527 train.py --workers 8 --device 0,1,2 " \
-              f"--sync-bn --batch-size ${batch_size} --data ${data_path} " \
-              f"--img ${img_size} --cfg ${cfg_path} --weights \'\' " \
+              f"--sync-bn --batch-size {batch_size} --data {data_path} " \
+              f"--img {img_size} --cfg {cfg_path} --weights \'\' " \
               f"--name yolov7 --hyp data/hyp.scratch.p5.yaml'"
 
-        cmd = f"python -m torch.distributed.launch " \
+        cmd = f"cd MetaHand/tools/yolov7/ && python -m torch.distributed.launch " \
               f"--nproc_per_node 3 --master_port 9527 train.py --workers 8 --device 0,1,2 " \
-              f"--sync-bn --batch-size ${batch_size} --data ${data_path} " \
-              f"--img ${img_size} --cfg ${cfg_path} --weights '' " \
+              f"--sync-bn --batch-size {batch_size} --data {data_path} " \
+              f"--img {img_size} --cfg {cfg_path} --weights '' " \
               f"--name yolov7 --hyp data/hyp.scratch.p5.yaml"
         subprocess.call(cmd, shell=True)
 
@@ -54,4 +54,5 @@ if __name__ == "__main__":
     container_name = "DNNTesting"
     dnnTest = DNNTest(container_name)
     # dnnTest.numerical_analysis("TensorFuzz.pbtxt")
-    dnnTest.detect_yolov7("./MetaHand/tools/yolov7/coco/images/val2017/000000289222.jpg", "./MetaHand/tools/yolov7/runs/train/yolov7/weights/best.pt")
+    # dnnTest.detect_yolov7("./MetaHand/tools/yolov7/coco/images/val2017/000000289222.jpg", "./MetaHand/tools/yolov7/runs/train/yolov7/weights/best.pt")
+    dnnTest.train_yolov7(data_path="/ssddata1/users/dlproj/ITF-Deliverables/MetaHand/tools/yolov7/pilotstudy/pilot.yaml")
