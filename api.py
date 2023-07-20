602b83692a64a2b47d8b24958cd3f25db12ec9c2
maybeLee <maybelee98@gmail.com>
2023-07-20 10:54:04 +0800

    Implement the model training api

--- api.py
+++ api.py
@@ -29,9 +29,23 @@ class DNNTest(object):
               f"--jobs=8"
         subprocess.call(cmd, shell=True)
 
-    def train_yolov7(self, train_path, valid_path):
-        pass
-    
+    def train_yolov7(self, data_path="/root/MetaHand/tools/yolov7/data/coco.yaml", img_size=320, batch_size=66, cfg_path="cfg/training/yolov7.yaml"):
+        # The path can be an absolute path or relative path with the root to be ./MetaHand/tools/yolov7
+        if not os.path.exists(data_path):
+            raise ValueError(f"The data path: {data_path} does not exist!")
+        cmd = f"docker exec {self.container_name}  /bin/sh -c 'cd MetaHand && python -m torch.distributed.launch " \
+              f"--nproc_per_node 3 --master_port 9527 train.py --workers 8 --device 0,1,2 " \
+              f"--sync-bn --batch-size ${batch_size} --data ${data_path} " \
+              f"--img ${img_size} --cfg ${cfg_path} --weights \'\' " \
+              f"--name yolov7 --hyp data/hyp.scratch.p5.yaml'"
+
+        cmd = f"python -m torch.distributed.launch " \
+              f"--nproc_per_node 3 --master_port 9527 train.py --workers 8 --device 0,1,2 " \
+              f"--sync-bn --batch-size ${batch_size} --data ${data_path} " \
+              f"--img ${img_size} --cfg ${cfg_path} --weights '' " \
+              f"--name yolov7 --hyp data/hyp.scratch.p5.yaml"
+        subprocess.call(cmd, shell=True)
+
     def mutate_image(self, img_dir):
         pass
 
