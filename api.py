443c4acf6fd13f07ce5cd091936ee86aecd25d26
maybeLee <maybelee98@gmail.com>
2023-07-20 19:27:16 +0800

    Update

--- api.py
+++ api.py
@@ -29,13 +29,13 @@ class DNNTest(object):
               f"--jobs=8"
         subprocess.call(cmd, shell=True)
 
-    def train_yolov7(self, proj_name="pilotstudy", data_path="/root/MetaHand/tools/yolov7/pilotstudy/data.yaml", img_size=640, batch_size=66, cfg_path="cfg/training/yolov7.yaml"):
+    def train_yolov7(self, proj_name="pilotstudy", data_path="/root/MetaHand/tools/yolov7/pilotstudy/data.yaml", img_size=640, batch_size=42, num_workers=4, cfg_path="cfg/training/yolov7.yaml"):
         # The path can be an absolute path or relative path with the root to be ./MetaHand/tools/yolov7
         if not os.path.exists(data_path):
             if not os.path.exists(data_path.replace("/root", os.getcwd())):
                 raise ValueError(f"The data path: {data_path} does not exist!")
         cmd = f'podman exec {self.container_name}  /bin/sh -c \'cd MetaHand/tools/yolov7 && /opt/conda/envs/metahand/bin/python -m torch.distributed.launch ' \
-              f'--nproc_per_node 3 --master_port 9527 train.py --workers 8 --device 0,1,2 ' \
+              f'--nproc_per_node 3 --master_port 9527 train.py --workers {num_workers} --device 1,0,2 ' \
               f'--sync-bn --batch-size {batch_size} --data {data_path} ' \
               f'--img {img_size} --cfg {cfg_path} --weights "" ' \
               f'--name {proj_name} --hyp data/hyp.scratch.p5.yaml\''
