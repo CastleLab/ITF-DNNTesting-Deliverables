1a727c66f8ca1bd834e15f2dec90fb96fb9ff57f
maybeLee <maybelee98@gmail.com>
2023-07-22 00:28:03 +0800

    Test api for image detection

--- api.py
+++ api.py
@@ -14,20 +14,17 @@ class DNNTest(object):
         return result
 
     def detect_yolov7(self, img_path, weights_path, size=320, confidence=0.25):
-        cmd = f"docker exec {self.container_name}  /bin/sh -c 'cd MetaHand && python -u -m scripts.evaluation.detect_parallel_yolov7" \
+        assert img_path.endswith(".jpg")
+        cmd = f"docker exec {self.container_name}  /bin/sh -c 'cd MetaHand && CONDA_PREFIX=/opt/conda/envs/metahand PATH=/opt/conda/envs/metahand/bin:$PATH /opt/conda/envs/metahand/bin/python -m scripts.evaluation.detect_parallel_yolov7 " \
               f"--img_dir {img_path} " \
               f"--weights_path {weights_path} " \
               f"--size {size} " \
               f"--confidence {confidence} " \
               f"--jobs=8'"
-
-        cmd = f"cd MetaHand && python -u -m scripts.evaluation.detect_parallel_yolov7 " \
-              f"--img_dir {img_path} " \
-              f"--weights_path {weights_path} " \
-              f"--size {size} " \
-              f"--confidence {confidence} " \
-              f"--jobs=8"
         subprocess.call(cmd, shell=True)
+        img_name = os.path.basename(img_path)
+        res_path = f"./MetaHand/tools/yolov7/runs/detect/{img_name}/{img_name}"
+        return res_path
 
     def train_yolov7(self, proj_name="pilotstudy", data_path="/root/MetaHand/tools/yolov7/pilotstudy/data.yaml", img_size=640, batch_size=42, num_workers=4, cfg_path="cfg/training/yolov7.yaml"):
         # The path can be an absolute path or relative path with the root to be ./MetaHand/tools/yolov7
@@ -49,5 +46,6 @@ if __name__ == "__main__":
     container_name = "DNNTesting"
     dnnTest = DNNTest(container_name)
     # dnnTest.numerical_analysis("TensorFuzz.pbtxt")
-    # dnnTest.detect_yolov7("./MetaHand/tools/yolov7/coco/images/val2017/000000289222.jpg", "./MetaHand/tools/yolov7/runs/train/yolov7/weights/best.pt")
-    dnnTest.train_yolov7(proj_name="pilotstudy", data_path="/root/MetaHand/tools/yolov7/pilotstudy/data.yaml")
+    path = dnnTest.detect_yolov7("/root/MetaHand/tools/yolov7/pilotstudy/images/val/ff1af9a2-frame2811.jpg", "/root/MetaHand/tools/yolov7/runs/train/pilotstudy/weights/best.pt")
+    print(path)
+    # dnnTest.train_yolov7(proj_name="pilotstudy", data_path="/root/MetaHand/tools/yolov7/pilotstudy/data.yaml")
